4c0d2acadc9c31bc2e3d45818c6f975f5ba6f50f
RANNIE CLAIRE <clairecardino15@gmail.com>
2017-03-19 23:23:12 +0800

    hello

---
+++ public/js/up2.js
@@ -0,0 +1,40 @@
+$('#app').html(`
+
+<div class="loader">Loading...</div>
+`);
+setTimeout(function(){
+	x();
+},1000);
+
+function x(){
+$.ajax({
+	url: "http://www.omdbapi.com/?i=tt1023481"
+}).done(function(res){
+	console.log(res);
+	let html = `
+<center>
+		<h1>${res.Title}</h1>
+		<img src="${res.Poster}"/>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/5CxCeZwZnfE" frameborder="0" allowfullscreen></iframe>
+		<div id="container">
+		<div id="center">
+		</div>
+		<h3>Type: ${res.Type}</h3>
+		<h3>Released: ${res.Released}</h3>
+		<h3>Runtime: ${res.Runtime}</h3>
+		<h3>Genre: ${res.Genre}</h3>
+		<h3>Director: ${res.Director}</h3>
+		<h3>Writer: ${res.Writer}</h3>
+		<h3>Actors: ${res.Actors}</h3>
+		<h3>Plot: ${res.Plot}</h3>
+		<h3>Language: ${res.Language}</h3>
+		<h3>Country: ${res.Country}</h3>
+		<h3>Awards: ${res.Awards}</h3>
+
+		
+	`;
+	$('#app').html(html);
+
+
+});
+}
